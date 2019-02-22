bf3ba525431b3005affbc42a3105208baeee0cb7
Angel Sanquiche <angelsan720@gmail.com>
2019-02-22 08:25:30 -0400

    update

--- filter-reads.jl
+++ filter-reads.jl
@@ -3,40 +3,65 @@ using BioSequences
 using CSV
 using DataFrames
 
-function filter_fasta(inname, outname, kmers)
+function filter_fasta(filename, kmers , condition)
 
-    reader = FASTA.Reader(open(inname, "r"))
-    writer = FASTA.Writer(open(outname, "w" ))
+    if occursin("FASTQ" , uppercase(filename))
+        if VERBOSE || DEBUG
+            println("I/O FASTQ")
+        end
+        reader = FASTQ.Reader(open(filename, "r"))
+        writer = FASTQ.Writer(open(filename + "filtered.FASTQ", "w" ))
+    else
+        if VERBOSE || DEBUG
+            println("I/O FASTA")
+        end
+        reader = FASTA.Reader(open(filename, "r"))
+        writer = FASTA.Writer(open(filename + "filtered.FASTA", "w" ))
+    end
     for record in reader
         # Do something
         eachkmer = [kmer for (_, kmer) in each(DNAKmer{13}, sequence(record))]
         canks = [convert(String, canonical(kmer)) for kmer in eachkmer]
         # use any or all to select reads that pass
 	# Anonymous function to check if all kmers are present
-        if (all(cank -> haskey(kmers, cank), canks))
-            write(writer, record)
+        if allany
+            if (all(cank -> haskey(kmers, cank), canks))
+                write(writer, record)
+            end
+        else
+            if (any(cank -> haskey(kmers, cank), canks))
+                write(writer, record)
+            end
         end
+
     end
 
     close(reader)
     close(writer)
 end
-
-function filter_files(df, kmers)
-
-    for rowi in 1:nrow(df)
-        sample_name = df[rowi,:rep_id]
-        println(sample_name)
-        forward_name = "simulated_reads/" * sample_name * "_1.fasta"
-        forward_out = sample_name * "_1.filtered.fa"
-        reverse_out = sample_name * "_2.filtered.fa"
-        reverse_name = "simulated_reads/" * sample_name * "_2.fasta"
-        filter_fasta(forward_name, forward_out, kmers)
-        filter_fasta(reverse_name, reverse_out, kmers)
+###############################################
+function filter_files(df , kmers , datadir , allany)
+    files = readdir(datadir)
+    for file in files
+        samp = joinpath(datadir , file)
+        filter_fasta(samp , kmers , allany)
     end
 end
+###############################################
+#function filter_files(df, kmers)
+#
+#    for rowi in 1:nrow(df)
+#        #sample_name = df[rowi,:rep_id]
+#        println(sample_name)
+#        forward_name = "simulated_reads/" * sample_name * "_1.fasta"
+#        forward_out = sample_name * "_1.filtered.fa"
+#        reverse_out = sample_name * "_2.filtered.fa"
+#        reverse_name = "simulated_reads/" * sample_name * "_2.fasta"
+#        filter_fasta(in, out, kmers)
+#    end
+#end
 
-function filter_reads(datafile , delimiter , diffkmers)
+function filter_reads(datafile , delimiter , diffkmers ,datadir, allany)
 	if DEBUG
 		println("Running filter_reads")
 	end
@@ -50,7 +75,7 @@ function filter_reads(datafile , delimiter , diffkmers)
 		kmers[kmer] = 1
 	end
 
-	filter_files(df, kmers)
+	filter_files(df , kmers , datadir , allany)
 end
 
 dic = Dict( "DEBUG"=>"false",
@@ -61,8 +86,13 @@ dic = Dict( "DEBUG"=>"false",
             "out_delim"=>"\t",
             "frame_delimiter"=>"\t",
             "dataframe"=>"",
-            "diffkmers"=>"diffkmers.txt" )
+            "datadir"=>"",
+            "diffkmers"=>"diffkmers.txt",
+            "allany"=>"true")
 
 dic = loadARGS(dic)
 
-filter_reads(dic["dataframe"] , dic["out_delim"] , dic["diffkmers"])
+DEBUG = dic["DEBUG"]=="true"
+VERBOSE = dic["VERBOSE"]=="true"
+
+filter_reads(dic["dataframe"] , dic["out_delim"] , dic["diffkmers"] , dic["datadir"],dic["allany"]=="all")
